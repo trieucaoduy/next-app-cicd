d21b459a248a87a92974210b966c9c82b152c5d9
trieucd <trieucd@runsystem.net>
2024-07-22 11:51:15 +0700

    chore: test lint

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 import React from 'react';
  
-export default function HomePage() {
+export default function HomePage(a: any) {
   return (
     <div>
       <h1>Home</h1>
-      <Link href="/about" >About</Link>
+      <Link href="/about">About</Link>
     </div>
   )
 }
